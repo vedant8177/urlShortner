5625c180ae1057365e83feb82ad7e7ed2641b36b
vedant kale <81308923+vedant8177@users.noreply.github.com>
2022-12-27 14:04:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,39 +10,55 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${__dirname}/public`));
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+  
+});
+
+
+
 
 const urls = [];
 
 app.post('/api/shorturl', (req, res) => {
-  let url = req.body.url.replace(/\/*$/, '');
-  let validUrl = url.replace(/^https:\/\/(www.)?/, '');
-  dns.lookup(validUrl, (err, address, family) => {
+  let url = new URL(req.body.url);
+  console.log(url);
+  dns.lookup(url.hostname, (err, address, family) => {
     if (err) {
       res.json({ error: 'invalid url' })
     }
     else {
-      if (!urls.includes(url)) {
-        urls.push(url);
+      if (!urls.includes(url.href)) {
+        urls.push(url.href);
       }
       res.json({
         original_url: url,
-        short_url: urls.indexOf(url) + 1
+        short_url: urls.indexOf(url.origin) + 1
       });
     }
   });
 });
 
-app.get('/api/shorturl/:id', (req, res) => {
-  const externarlUrl = urls[req.params.id - 1];
-  res.redirect(externarlUrl);
-});
+
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(id)
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
